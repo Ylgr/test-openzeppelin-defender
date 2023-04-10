7dd8fd03a9a4af3464c2099cb8278adabe89a266
viet tu <quyen.luminous@gmail.com>
2023-04-10 18:01:10 +0700

    (+) upgradeable deployer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,35 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-require('@openzeppelin/hardhat-upgrades');
+import '@openzeppelin/hardhat-upgrades';
+import '@openzeppelin/hardhat-defender';
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      // accounts: [process.env.PRIVATE_KEY as string],
+      accounts: [process.env.PRIVATE_KEY_2 as string],
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: [process.env.PRIVATE_KEY as string],
+    }
+  },
+  defender: {
+    apiKey: process.env.DEFENDER_API_KEY as string,
+    apiSecret: process.env.DEFENDER_SECRET_KEY as string
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,27 @@
-import { ethers } from "hardhat";
-
+import { ethers, upgrades, defender } from "hardhat";
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const CustomToken = await ethers.getContractFactory("CustomToken");
+  const initialSupply = 1000;
+  const mintInterval = 60 * 60 * 24; // 1 day
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  console.log("Deploying CustomToken...");
+  const customToken = await upgrades.deployProxy(CustomToken, ["CustomToken", "CTK", initialSupply, mintInterval], { initializer: "initialize" });
+  await customToken.deployed();
+  console.log("CustomToken deployed to:", customToken.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await proposeUpgrade(customToken.address);
+  // await proposeUpgrade('0xd25625B8Df6204037Ff0Ef82dbc2F39741e3A0D4');
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function proposeUpgrade(customTokenAddress: string) {
+  const CustomTokenV2 = await ethers.getContractFactory("CustomTokenV2");
+  const proposal = await defender.proposeUpgrade(customTokenAddress, CustomTokenV2);
+  console.log("Upgrade proposal created at:", proposal.url);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
