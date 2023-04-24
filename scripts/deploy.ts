368b5846f48961919d9b6883e7178bdbb8058da0
viet tu <quyen.luminous@gmail.com>
2023-04-24 11:27:41 +0700

    (+) Token meta tx deployer

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,66 @@
 import { ethers, upgrades, defender } from "hardhat";
+import {writeFileSync} from "fs";
+const {
+    DefenderRelayProvider,
+    DefenderRelaySigner,
+} = require('defender-relay-client/lib/ethers')
 async function main() {
-  const CustomToken = await ethers.getContractFactory("CustomToken");
-  const initialSupply = 1000;
-  const mintInterval = 60 * 60 * 24; // 1 day
+    require('dotenv').config()
+    const credentials = {
+        apiKey: process.env.RELAYER_API_KEY,
+        apiSecret: process.env.RELAYER_API_SECRET,
+    }
+    const provider = new DefenderRelayProvider(credentials)
+    const relaySigner = new DefenderRelaySigner(credentials, provider, {
+        speed: 'fast',
+    })
+  //
+  // const CustomToken = await ethers.getContractFactory("CustomToken");
+  // const TokenMetaTx = await ethers.getContractFactory("TokenMetaTx");
+  // const initialSupply = 1000;
+  // const mintInterval = 60 * 60 * 24; // 1 day
+  //
+  // console.log("Deploying CustomToken...");
+  // const customToken = await upgrades.deployProxy(CustomToken, ["CustomToken", "CTK", initialSupply, mintInterval], { initializer: "initialize" });
+  // // const customToken = await ethers.deployContract("CustomToken")
+  // await customToken.deployed();
+  // console.log("CustomToken deployed to:", customToken.address);
+  //   // await proposeUpgrade(customToken.address);
+  //   // await proposeUpgrade('0xd25625B8Df6204037Ff0Ef82dbc2F39741e3A0D4');
+    const initialSupply = ethers.utils.parseEther('1000')
+    const Forwarder = await ethers.getContractFactory('MinimalForwarder')
+    const forwarder = await Forwarder.connect(relaySigner)
+        .deploy()
+        .then((f) => f.deployed())
 
-  console.log("Deploying CustomToken...");
-  const customToken = await upgrades.deployProxy(CustomToken, ["CustomToken", "CTK", initialSupply, mintInterval], { initializer: "initialize" });
-  // const customToken = await ethers.deployContract("CustomToken")
-  await customToken.deployed();
-  console.log("CustomToken deployed to:", customToken.address);
+    const TokenMetaTx = await ethers.getContractFactory('TokenMetaTx')
+    const tokenMetaTx = await TokenMetaTx.connect(relaySigner)
+        .deploy(initialSupply, forwarder.address)
+        .then((f) => f.deployed())
 
-  // await proposeUpgrade(customToken.address);
-  // await proposeUpgrade('0xd25625B8Df6204037Ff0Ef82dbc2F39741e3A0D4');
+
+    writeFileSync(
+        'deploy.json',
+        JSON.stringify(
+            {
+                MinimalForwarder: forwarder.address,
+                TokenMetaTx: tokenMetaTx.address,
+            },
+            null,
+            2
+        )
+    )
+
+    console.log(
+        `MinimalForwarder: ${forwarder.address}\nTokenMetaTx: ${tokenMetaTx.address}`
+    )
 }
 
-async function proposeUpgrade(customTokenAddress: string) {
-  const CustomTokenV2 = await ethers.getContractFactory("CustomTokenV2");
-  const proposal = await defender.proposeUpgrade(customTokenAddress, CustomTokenV2);
-  console.log("Upgrade proposal created at:", proposal.url);
-}
+// async function proposeUpgrade(customTokenAddress: string) {
+//   const CustomTokenV2 = await ethers.getContractFactory("CustomTokenV2");
+//   const proposal = await defender.proposeUpgrade(customTokenAddress, CustomTokenV2);
+//   console.log("Upgrade proposal created at:", proposal.url);
+// }
 
 main()
     .then(() => process.exit(0))
