ef1d25f8d422d0f074092b28f68d1ff40764612d
viet tu <quyen.luminous@gmail.com>
2023-04-26 15:44:29 +0700

    (+) basic case mint nft

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,25 +3,34 @@ import './App.css';
 import {Button} from "react-bootstrap";
 import { ethers } from "ethers";
 import SimpleNftAbi from "./abi/SimpleNft.json";
+import SimpleNftFactoryAbi from "./abi/SimpleNftFactory.json";
 
 function App() {
     // @ts-ignore
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const contract = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", SimpleNftAbi, signer);
-
+    const nftContract = new ethers.Contract(process.env.REACT_APP_SIMPLE_NFT_FACTORY_ADDRESS as string, SimpleNftFactoryAbi, signer);
+    const [isConnected, setIsConnected] = React.useState(false);
     const createNft = async () => {
-        const tx = await contract.createNft();
+        console.log('contract: ', nftContract)
+        const tx = await nftContract.createNft("Test NFT Relayer", "TR", await signer.getAddress());
         console.log(tx);
     }
 
     const createNftZeroFee = async () => {
     }
 
-  return (
+    return (
     <div className="App">
-      <Button variant="primary" onClick={() => createNft()}>Create Nft</Button>
-      <Button variant="info" onClick={() => createNftZeroFee()}>Create Nft Zero fee</Button>
+
+        <Button variant="primary" onClick={() => {
+            // @ts-ignore
+            window.ethereum.request({method: 'eth_requestAccounts'})
+                .then(() => setIsConnected(true))
+        }} disabled={isConnected}>Connect</Button>
+        <br/>
+      <Button variant="primary" onClick={() => createNft()} disabled={!isConnected}>Create Nft</Button>
+      <Button variant="info" onClick={() => createNftZeroFee()} disabled={!isConnected}>Create Nft Zero fee</Button>
     </div>
   );
 }
